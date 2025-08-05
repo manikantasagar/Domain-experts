00ec8b1c286825fb10978329272807064c9e6a5e
manikantasagar <161570279+manikantasagar@users.noreply.github.com>
2025-08-05 12:55:02 +0530

    Update Login.js

--- client/src/Login.js
+++ client/src/Login.js
@@ -18,7 +18,7 @@ function Login() {
     console.log(process.env.REACT_APP_SERVER_URL)
     console.log(form)
     try {
-      const response = await fetch(`https://domain-experts.onrender.com/home/logins/`, {
+      const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/home/logins/`, {
         method: 'POST',      
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
