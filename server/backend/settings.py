7578f7ccf7bb75c9f89359a1804b26e82c086db3
manikantasagar <manikantasagar31@gmail.com>
2025-08-12 22:00:07 +0530

    up

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -60,7 +60,7 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
 }
-
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # CORS Settings
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
